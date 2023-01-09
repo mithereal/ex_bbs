c4d30b7a434454655855ccd20b7d5013a391370b
mithereal <mithereal@gmail.com>
2023-01-09 00:52:54 -0700

    add vm args

--- priv/repo/migrations/20220830001621_create_settings.exs
+++ priv/repo/migrations/20220830001621_create_settings.exs
@@ -2,7 +2,8 @@ defmodule Api.Repo.Migrations.CreateSettings do
   use Ecto.Migration
 
   def change do
-    create table(:bbs_settings) do
+    create table(:bbs_settings, primary_key: false) do
+      add :id, :uuid, primary_key: true
       add :name, :string
       add :key, :string
       add :value, :string
