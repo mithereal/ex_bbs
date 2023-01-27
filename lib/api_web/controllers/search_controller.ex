cd5df8d6cda407c16243b93f2d5777e8baf3c1d5
mithereal <mithereal@gmail.com>
2023-01-27 05:03:38 -0700

    breadcrumb layout

--- lib/api_web/controllers/search_controller.ex
+++ lib/api_web/controllers/search_controller.ex
@@ -1,6 +1,8 @@
 defmodule ApiWeb.SearchController do
   use ApiWeb, :controller
+  use Breadcrumble
 
+  plug :add_breadcrumb, name: 'top', url: '/'
 
   def index(conn, params) do
     render(conn, "index.html")
