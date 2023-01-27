45de13bcff06e3f0c117337848b4ee8c3d262bd5
mithereal <mithereal@gmail.com>
2023-01-26 23:18:24 -0700

    setup status

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -7,18 +7,18 @@ config = url = Application.get_env(:api, ApiWeb.Endpoint)
     type: "system"
   })
 
-{:ok, user_1} =
+{:ok, user} =
   Api.Accounts.register_user(%{
-    email: "user1@" <> hostname,
-    password: "123456789abc",
-    password_confirmation: "123456789abc",
-    username: "user1",
+    email: "demo@" <> hostname,
+    password: "demo",
+    password_confirmation: "demo",
+    username: "demo",
     terms: true,
     active: true,
     confirmed_at: DateTime.utc_now()
   })
 
-{:ok, user_2} =
+{:ok, _user_2} =
   Api.Accounts.register_user(%{
     email: "user2@" <> hostname,
     password: "123456789abc",
@@ -29,7 +29,7 @@ config = url = Application.get_env(:api, ApiWeb.Endpoint)
     confirmed_at: DateTime.utc_now()
   })
 
-{:ok, user_3} =
+{:ok, _user_3} =
   Api.Accounts.register_user(%{
     email: "user3@" <> hostname,
     password: "123456789abc",
@@ -40,41 +40,41 @@ config = url = Application.get_env(:api, ApiWeb.Endpoint)
     confirmed_at: DateTime.utc_now()
   })
 
-{:ok, category_1} =
+{:ok, category} =
   Api.Forum.create_categories(%{
-    description: "Test Category Description",
+    description: "Description of your First Category",
     order: 1,
     status: status,
-    title: "Test Category",
-    user: user_3
+    title: "Your First Category",
+    user: user
   })
 
-{:ok, forum_1} =
+{:ok, forum} =
   Api.Forum.create_forums(%{
-    description: "Test Forum Description",
+    description: "Description of your First Forum.",
     order: 1,
     status: status,
-    title: "Test Forum",
-    category: category_1,
-    user: user_3
+    title: "Your First Forum.",
+    category: category,
+    user: user
   })
 
-{:ok, topic_1} =
+{:ok, topic} =
   Api.Forum.create_topics(%{
-    description: "Test Topic Description",
+    description: "Welcome to exBBS",
     status: status,
-    title: "Test Topic",
-    user: user_3,
-    forum: forum_1
+    title: "Welcome to exBBS",
+    user: user,
+    forum: forum
   })
 
-{:ok, post_1} =
+{:ok, post} =
   Api.Forum.create_posts(%{
-    body: "Test Post Description",
-    description: "Test Post Description",
+    body: "This is an example post in your exBBS installation.",
+    description:  "Welcome to exBBS",
     status: status,
-    title: "Test Post",
-    user: user_3,
-    forum: forum_1,
-    topic: topic_1
+    title:  "Welcome to exBBS",
+    user: user,
+    forum: forum,
+    topic: topic
   })
