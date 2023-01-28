a4254166f1fa9d4593ae1b705f26ae0995253a43
mithereal <mithereal@gmail.com>
2023-01-27 22:04:45 -0700

    add csp

--- lib/api_web/endpoint.ex
+++ lib/api_web/endpoint.ex
@@ -1,7 +1,7 @@
 defmodule ApiWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :api
 
-  # plug PlugContentSecurityPolicy
+  plug PlugContentSecurityPolicy
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
